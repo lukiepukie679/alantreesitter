a788f926a05418f252f66bfd3895bd9492f25bf6
Luke John Payne <24736880@sun.ac.za>
2024-05-29 15:13:06 +0200

    highlight queries

---
+++ queries/highlights.scm
@@ -0,0 +1,29 @@
+;; Highlights for Alan language
+
+((source) :foreground "#0000ff") ;; Top-level source code in blue
+
+((function) :foreground "#800080") ;; Functions in purple
+
+((body) :foreground "#000000") ;; Function bodies in black
+
+((type) :foreground "#008000") ;; Types in green
+
+((variable_definition) :foreground "#000000") ;; Variable definitions in black
+
+((statements) :foreground "#000000") ;; Statements in black
+
+((statement) :foreground "#000000") ;; Individual statements in black
+
+((expression) :foreground "#ff0000") ;; Expressions in red
+
+((comment) :foreground "#808080") ;; Comments in gray
+
+((TRUE) :foreground "#008000") ;; TRUE keyword in green
+
+((FALSE) :foreground "#008000") ;; FALSE keyword in green
+
+((identifier) :foreground "#000000") ;; Identifiers in black
+
+((number) :foreground "#0000ff") ;; Numbers in blue
+
+((string) :foreground "#800000") ;; Strings in maroon
